e724156b9906457f5a61f12405ac2b2f1c34fc83
denyss <denhnure@gmail.com>
2021-01-16 17:24:22 +0000

    Creating configured BooleanToVisibilityConverter

---
+++ Hospital/ValueConverters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Hospital.ValueConverters
+{
+    public sealed class BooleanToVisibilityConverter : BooleanConverter<Visibility>
+    {
+        public BooleanToVisibilityConverter() :
+            base(Visibility.Visible, Visibility.Collapsed)
+        { }
+    }
+}
